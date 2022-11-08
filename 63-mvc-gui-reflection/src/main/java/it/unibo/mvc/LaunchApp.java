aa58e004468845306e05ecfbce6118220e927ad5
Lorenzo Prati <lorenzo.prati3@studio.unibo.it>
2022-11-08 17:45:29 +0100

    (63) Complete Multiple Views

--- 63-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
+++ 63-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
@@ -3,6 +3,7 @@ package it.unibo.mvc;
 import it.unibo.mvc.api.DrawNumberController;
 import it.unibo.mvc.controller.DrawNumberControllerImpl;
 import it.unibo.mvc.model.DrawNumberImpl;
+import it.unibo.mvc.view.DrawNumberStdoutView;
 import it.unibo.mvc.view.DrawNumberSwingView;
 
 /**
@@ -10,22 +11,24 @@ import it.unibo.mvc.view.DrawNumberSwingView;
  */
 public final class LaunchApp {
 
-    private LaunchApp() { }
+    private LaunchApp() {
+    }
 
     /**
      * Runs the application.
      *
      * @param args ignored
-     * @throws ClassNotFoundException if the fetches class does not exist
-     * @throws NoSuchMethodException if the 0-ary constructor do not exist
+     * @throws ClassNotFoundException    if the fetches class does not exist
+     * @throws NoSuchMethodException     if the 0-ary constructor do not exist
      * @throws InvocationTargetException if the constructor throws exceptions
-     * @throws InstantiationException if the constructor throws exceptions
-     * @throws IllegalAccessException in case of reflection issues
-     * @throws IllegalArgumentException in case of reflection issues
+     * @throws InstantiationException    if the constructor throws exceptions
+     * @throws IllegalAccessException    in case of reflection issues
+     * @throws IllegalArgumentException  in case of reflection issues
      */
     public static void main(final String... args) {
         final var model = new DrawNumberImpl();
         final DrawNumberController app = new DrawNumberControllerImpl(model);
         app.addView(new DrawNumberSwingView());
+        app.addView(new DrawNumberStdoutView());
     }
 }
---
+++ 63-mvc-gui-reflection/src/main/java/it/unibo/mvc/view/DrawNumberStdoutView.java
@@ -0,0 +1,25 @@
+package it.unibo.mvc.view;
+
+import it.unibo.mvc.api.DrawNumberController;
+import it.unibo.mvc.api.DrawNumberView;
+import it.unibo.mvc.api.DrawResult;
+
+public class DrawNumberStdoutView implements DrawNumberView {
+
+    @Override
+    public void setController(final DrawNumberController observer) {
+        /*
+         * Nothing to do
+         */
+    }
+
+    @Override
+    public void start() {
+        System.out.println("StdoutView start");
+    }
+
+    @Override
+    public void result(final DrawResult res) {
+        System.out.println(res.getDescription());
+    }
+}
